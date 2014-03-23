5ac72b70013286317976ef71ecdae52d2385f02e
rickytan <rickytan@users.noreply.github.com>
2014-03-23 12:45:38 +0800

    Update RTSiderViewController.m

--- Classes/RTSiderViewController.m
+++ Classes/RTSiderViewController.m
@@ -690,7 +690,7 @@
                          if (finished)
                              self.state = SlideStateMiddle;
                          _currentMiddleViewController.view.userInteractionEnabled = YES;
-                         [_currentLeftViewController viewDidAppear:YES];
+                         [_currentMiddleViewController viewDidAppear:YES];
                      }];
 }
 
